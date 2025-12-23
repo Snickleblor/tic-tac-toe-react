5fdd63b9b8b920b0550ff172a58e67a48ffce161
simon <simonbakkenjantasuk@pm.me>
2025-12-23 01:35:35 +0100

    Add PRODUCTION_WSS_URL

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,9 @@
-import { io } from 'socket.io-client';
+import { io } from "socket.io-client";
 
-const socketURL = process.env.NODE_ENV === "production" ? `wss://mysterious-ravine-23326-f566692a40b9.herokuapp.com/` : "http://localhost:4000"; 
+const socketURL =
+  process.env.NODE_ENV === "production"
+    ? process.env.PRODUCTION_WSS_URL
+    : "http://localhost:4000";
 
 export const socket = io(socketURL);
+
